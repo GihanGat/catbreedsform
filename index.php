8dbf4549ef0f05a472d47f9d8d442a4959ed6bb1
GihanGat <6135528499+GihanGat@users.noreply.github.com>
2020-03-26 11:02:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ switch ($request) {
     //     break;
     case '/form' :
         require 'form.php';
-        break
+    break;
     default:
         http_response_code(404);
         //require __DIR__ . '/views/404.php';
